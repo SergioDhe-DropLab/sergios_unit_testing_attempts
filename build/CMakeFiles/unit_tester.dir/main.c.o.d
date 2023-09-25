086526faa5eb1821eda0e24be03a72b874b14a3e
SergioDhe-DropLab <134008599+SergioDhe-DropLab@users.noreply.github.com>
2023-09-25 10:44:21 -0400

    ingored some stuff

--- build/CMakeFiles/unit_tester.dir/main.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/unit_tester.dir/main.c.o: \
- /home/sergio/projects/sergios_unit_testing_attempts/main.c \
- /usr/include/stdc-predef.h \
- /home/sergio/projects/sergios_unit_testing_attempts/src/number_generator.h \
- /home/sergio/projects/sergios_unit_testing_attempts/src/number_parser.h \
- /home/sergio/projects/sergios_unit_testing_attempts/src/user_io.h
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sergio/projects/sergios_unit_testing_attempts
-# Build directory: /home/sergio/projects/sergios_unit_testing_attempts/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
